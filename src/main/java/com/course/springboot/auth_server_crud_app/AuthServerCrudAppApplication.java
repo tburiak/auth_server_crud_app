7ea32d2398a547a4c7545bbb3d40efe31a489f6e
Taras Buriak <tarasburyak@gmail.com>
2022-11-10 20:57:22 +0200

    Initial commit

---
+++ src/main/java/com/course/springboot/auth_server_crud_app/AuthServerCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.course.springboot.auth_server_crud_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/springboot/auth_server_crud_app/AuthServerCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.springboot.auth_server_crud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
